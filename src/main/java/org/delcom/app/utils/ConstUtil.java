1cc5246489cc4ae4ca5bad1ebf7a660512cc3cb3
sandrinashe <sandrinatrianisa@gmail.com>
2025-11-30 21:53:49 +0700

    lagii

--- src/main/java/org/delcom/app/utils/ConstUtil.java
+++ src/main/java/org/delcom/app/utils/ConstUtil.java
@@ -7,6 +7,10 @@ public class ConstUtil {
     public static final String TEMPLATE_PAGES_AUTH_LOGIN = "pages/auth/login";
     public static final String TEMPLATE_PAGES_AUTH_REGISTER = "pages/auth/register";
     public static final String TEMPLATE_PAGES_HOME = "pages/home";
+
+    // Variabel yang ditambahkan agar error hilang
+    public static final String TEMPLATE_PAGES_TODOS_HOME = "pages/todos/home";
+    public static final String TEMPLATE_PAGES_CASHFLOWS_HOME = "pages/cashflows/home";
+
     public static final String TEMPLATE_PAGES_TODOS_DETAIL = "pages/todos/detail";
-    public static final String TEMPLATE_PAGES_CASHFLOWS_HOME = null;
 }
